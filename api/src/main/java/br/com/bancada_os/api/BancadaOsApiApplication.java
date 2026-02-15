3a6c1a84f654656953568ebe6b7e36da0986a4d4
João Francisco <joaofrancisco.amello@gmail.com>
2026-02-15 18:25:22 -0300

    build: criação da build inicial do projeto

---
+++ api/src/main/java/br/com/bancada_os/api/BancadaOsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bancada_os.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancadaOsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancadaOsApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/br/com/bancada_os/api/BancadaOsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bancada_os.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancadaOsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
